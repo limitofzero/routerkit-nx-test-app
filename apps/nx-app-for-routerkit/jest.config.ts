a88b7d525c6aac1a51ff225be94fb82876c6719e
limitofzero <limitofzero@gmail.com>
2022-07-31 20:34:50 +0400

    chore(): update angular/nx

--- apps/nx-app-for-routerkit/jest.config.ts
+++ apps/nx-app-for-routerkit/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'nx-app-for-routerkit',
-  preset: '../../jest.preset.js',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -11,10 +11,15 @@ module.exports = {
   },
   coverageDirectory: '../../coverage/apps/nx-app-for-routerkit',
 
-  transform: { '^.+\\.(ts|js|html)$': 'jest-preset-angular' },
+  transform: {
+    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../jest.preset.ts',
 };
