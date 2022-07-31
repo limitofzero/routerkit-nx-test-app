f554b000f949c11442db1359d9b9a38bc0287043
limitofzero <limitofzero@gmail.com>
2022-07-31 20:54:59 +0400

    feat(): update to v14

--- apps/nx-app-for-routerkit/jest.config.ts
+++ apps/nx-app-for-routerkit/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'nx-app-for-routerkit',
 
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
@@ -21,5 +21,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
